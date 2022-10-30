993fc666ea7787f9eda4b0b31522ce876b0a2820
Sara <saraeliza.lirio@gmail.com>
2022-10-30 12:38:23 -0300

    Adicionando rotas da telas de navegação

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Search from './src/screens/Search';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native'
+
+import Menu from './src/screens/Menu';
+import CurrentWeather from './src/screens/CurrentWeather';
+import Forecast from './src/screens/Forecast';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <View style={styles.container}>
-        <Text>Oii</Text>
-        <Search/>        
-        <StatusBar style="auto" />
-      </View>
+    <NavigationContainer>
+      <StatusBar style="auto" backgroundColor='#E5E5E5' />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+        // options={{ title: 'Menu' }}
+        />
+        <Stack.Screen
+          name="CurrentWeather"
+          component={CurrentWeather}
+        // options={{ title: 'Search' }}
+        />
+        <Stack.Screen
+          name="Forecast"
+          component={Forecast}
+        // options={{ title: 'Search' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
